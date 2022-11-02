54be500315a7fd16d45d2c8fb9ae93bed4ba5e19
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-11-02 17:17:31 -0500

    Create twice_as_old.js

---
+++ codewars/8 kyu/twice_as_old.js
@@ -0,0 +1,7 @@
+//  https://www.codewars.com/kata/5b853229cfde412a470000d0/javascript
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - sonYearsOld * 2)
+}
+
+console.log(twiceAsOld(36, 7))
